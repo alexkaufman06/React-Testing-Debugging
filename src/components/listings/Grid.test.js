0b87d67674fd01cea773f395c77b27d112757a3f
Alex Kaufman <alexkaufman06@gmail.com>
2018-06-02 12:10:44 -0700

    Add object/array tests and gitignore file

--- src/components/listings/Grid.test.js
+++ src/components/listings/Grid.test.js
@@ -1,5 +1,6 @@
 import data from '../../data/courses.json';
 
+// Integers
 const numItems = data.length;
 
 test('Number of items = 12', () => {
@@ -10,6 +11,7 @@ test('Number of items to be >= 12', () => {
 	expect(numItems).toBeGreaterThanOrEqual(12);
 });
 
+// Strings
 const dataTest = data[0].title;
 
 test('There is a JS in this title', () => {
@@ -19,6 +21,22 @@ test('There is a JS in this title', () => {
 
 test('The title contains React', () => {
 	expect(dataTest).toContain('React');
+});
+
+// Arrays
+const dataArray = ['React Native', 'MeteorJS'];
+
+test('The array contains React native and MeteorJS', () => {
+	expect(['React Native', 'MeteorJS', 'Poop']).toEqual(expect.arrayContaining(dataArray));
+});
+
+// Objects
+test('The first course to have a property of Title', () => {
+	expect(data[0]).toHaveProperty('title');
+});
+
+test('The first course to have a 31,266 views', () => {
+	expect(data[0]).toHaveProperty('views', '31,266');
 });
 
 /* MORE MATCHERS METHODS CAN BE FOUND HERE:

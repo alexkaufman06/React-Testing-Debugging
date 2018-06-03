d89e93a3f26423dd02a6397a0380f311dee7169c
Alex Kaufman <alexkaufman06@gmail.com>
2018-06-03 10:18:40 -0700

    Organize tests with describe

--- src/components/listings/Grid.test.js
+++ src/components/listings/Grid.test.js
@@ -1,42 +1,46 @@
 import data from '../../data/courses.json';
 
-// Integers
+// INTEGER TESTS
 const numItems = data.length;
 
-test('Number of items = 12', () => {
-	expect(numItems).toBe(12);
+describe('Integer tests', () => {
+	test('Number of items = 12', () => {
+		expect(numItems).toBe(12);
+	});
+
+	test('Number of items to be >= 12', () => {
+		expect(numItems).toBeGreaterThanOrEqual(12);
+	});
 });
 
-test('Number of items to be >= 12', () => {
-	expect(numItems).toBeGreaterThanOrEqual(12);
-});
-
-// Strings
+// STRING TESTS
 const dataTest = data[0].title;
 
-test('There is a JS in this title', () => {
-	expect(dataTest).toMatch(/JS/);
-	// JS Portion has REGEX
+describe('String tests', () => {
+	test('There is a JS in this title', () => {
+		expect(dataTest).toMatch(/JS/); // JS Portion is REGEX
+	});
+
+	test('The title contains React', () => {
+		expect(dataTest).toContain('React');
+	});
 });
 
-test('The title contains React', () => {
-	expect(dataTest).toContain('React');
-});
-
-// Arrays
+// ARRAYS AND OBJECTS TESTS
 const dataArray = ['React Native', 'MeteorJS'];
 
-test('The array contains React native and MeteorJS', () => {
-	expect(['React Native', 'MeteorJS', 'Poop']).toEqual(expect.arrayContaining(dataArray));
-});
+describe('Arrays and Objects tests', () => {
+	test('The array contains React native and MeteorJS', () => {
+		expect(['React Native', 'MeteorJS', 'Poop']).toEqual(expect.arrayContaining(dataArray));
+	});
 
-// Objects
-test('The first course to have a property of Title', () => {
-	expect(data[0]).toHaveProperty('title');
-});
+	test('The first course to have a property of Title', () => {
+		expect(data[0]).toHaveProperty('title');
+	});
 
-test('The first course to have a 31,266 views', () => {
-	expect(data[0]).toHaveProperty('views', '31,266');
+	test('The first course to have a 31,266 views', () => {
+		expect(data[0]).toHaveProperty('views', '31,266');
+	});
 });
 
 /* MORE MATCHERS METHODS CAN BE FOUND HERE:
